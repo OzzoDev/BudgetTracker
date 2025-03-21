9aab0aa58052481bcb58a681ba54e03df8f1f945
oscar <oscar.burman005@gmail.com>
2025-03-21 18:06:03 +0100

    Made the charts responsive

--- src/components/statistics/Chart.jsx
+++ src/components/statistics/Chart.jsx
@@ -1,22 +1,24 @@
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 import CustomTooltip from "./CustomTooltip";
 import CustomXAxisTick from "./CustomXAxisTick";
 
 export default function Chart({ xAxisKey, yAxisKey, chartData, colorMap, messages }) {
   return (
-    <BarChart width={600} height={300} data={chartData}>
-      <CartesianGrid />
-      <XAxis
-        dataKey={xAxisKey}
-        tickCount={chartData.length}
-        interval={0}
-        tick={(props) => <CustomXAxisTick {...props} colorMap={colorMap} />}
-      />
-      <YAxis />
-      <Tooltip
-        content={<CustomTooltip messages={messages} chartData={chartData} xAxisKey={xAxisKey} />}
-      />
-      <Bar dataKey={yAxisKey} fill="#8884d8" />
-    </BarChart>
+    <ResponsiveContainer width="100%" height="100%">
+      <BarChart data={chartData}>
+        <CartesianGrid />
+        <XAxis
+          dataKey={xAxisKey}
+          tickCount={chartData.length}
+          interval={0}
+          tick={(props) => <CustomXAxisTick {...props} colorMap={colorMap} />}
+        />
+        <YAxis />
+        <Tooltip
+          content={<CustomTooltip messages={messages} chartData={chartData} xAxisKey={xAxisKey} />}
+        />
+        <Bar dataKey={yAxisKey} fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
   );
 }
--- src/components/statistics/ChartCard.jsx
+++ src/components/statistics/ChartCard.jsx
@@ -2,9 +2,9 @@ import Chart from "./Chart";
 
 export default function ChartCard({ headline, yAxisKey, xAxisKey, chartData, colorMap, messages }) {
   return (
-    <div className="flex flex-col items-center p-8 rounded-md w-full h-full bg-slate-800 overflow-x-hidden">
+    <div className="flex flex-col items-center p-8 rounded-md w-full h-full bg-slate-800 overflow-x-hidden w-50 h-50">
       <h2 className="text-2xl text-gray-400 mb-8">{headline}</h2>
-      <div className="pr-8">
+      <div className="pr-8 w-full h-full">
         <Chart
           xAxisKey={xAxisKey}
           yAxisKey={yAxisKey}
