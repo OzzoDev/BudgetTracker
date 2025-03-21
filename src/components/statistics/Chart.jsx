b1e823a75f5b0bc1b54ffc3adf03550be9f33a41
oscar <oscar.burman005@gmail.com>
2025-03-21 20:58:58 +0100

    Made bar charts fill with category color

--- src/components/statistics/Chart.jsx
+++ src/components/statistics/Chart.jsx
@@ -1,6 +1,5 @@
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+import { BarChart, Bar, YAxis, CartesianGrid, Tooltip, Cell, ResponsiveContainer } from "recharts";
 import CustomTooltip from "./CustomTooltip";
-import CustomXAxisTick from "./CustomXAxisTick";
 import { formatNumber } from "@/utils/helpers";
 
 export default function Chart({
@@ -15,21 +14,24 @@ export default function Chart({
     return value === 0 ? 0 : isCountChart ? formatNumber(value) : `$ ${formatNumber(value)}`;
   };
 
+  const getColor = (index) => {
+    const colorObj = colorMap.find((map) => map.category === index);
+    return colorObj ? colorObj.color : "#8884d8";
+  };
+
   return (
     <ResponsiveContainer width="100%" height="100%">
       <BarChart data={chartData}>
         <CartesianGrid />
-        <XAxis
-          dataKey={xAxisKey}
-          tickCount={chartData.length}
-          interval={0}
-          tick={(props) => <CustomXAxisTick {...props} colorMap={colorMap} />}
-        />
         <YAxis tickFormatter={(value) => formatYAxisTick(value)} />
         <Tooltip
           content={<CustomTooltip messages={messages} chartData={chartData} xAxisKey={xAxisKey} />}
         />
-        <Bar dataKey={yAxisKey} fill="#8884d8" />
+        <Bar dataKey={yAxisKey}>
+          {chartData.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={getColor(entry[xAxisKey])} />
+          ))}
+        </Bar>
       </BarChart>
     </ResponsiveContainer>
   );
--- src/components/statistics/CustomXAxisTick.jsx
+++
@@ -1,9 +0,0 @@
-export default function CustomXAxisTick({ x, y, payload, colorMap }) {
-  const color = colorMap.find((map) => map.category === payload.value).color;
-
-  return (
-    <g transform={`translate(${x - 12},${y + 10})`}>
-      <rect width={24} height={5} fill={color} />
-    </g>
-  );
-}
